5ca44816f911da35ef13c0d3ce4fd3aaf82be1f9
jztimm <jztimm@gmail.com>
2020-07-13 22:06:45 -0400

    added dog and person

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class dog
+
+end
+
+fido = dog.new
+snoopy = dog.new
+lassie = dog.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+
+class person
+
+end
+
+adele_goldberg = person.new
+
+alan_kay = person.new
